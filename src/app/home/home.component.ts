cbdd36aec50209c96b6d745fdc9047ed4b205990
Sitaram <sitaram075@gmail.com>
2023-09-17 10:20:26 +0530

    first commit

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+declare var Calendly: any;
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  scheduleCall() {
+    console.log('Schedule');
+    Calendly.initPopupWidget({url: 'https://calendly.com/auxo-advisors/financial-advice'});
+    return false;
+  }
+}
